2d9da4db9a40229567ba25b78ca3ad15837ff8ef
Will Johnston <will-github@wwj.dev>
2024-12-18 13:26:14 -0800

    feat: CRUD test done

--- pkg/todos/main_test.go
+++ pkg/todos/main_test.go
@@ -4,10 +4,19 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/redis-developer/redis-starter-go/cmd/config"
 	"github.com/redis-developer/redis-starter-go/pkg/redis"
 )
+
+func printTodo(todo *Todo) string {
+	return fmt.Sprintf("id:%s name:%s status:%s", todo.ID, todo.Name, todo.Status)
+}
+
+func todosEqual(t1 *Todo, t2 *Todo) bool {
+	return t1.ID == t2.ID && t1.Name == t2.Name && t1.Status == t2.Status
+}
 
 func TestCrud(t *testing.T) {
 	cfg := config.Config{}
@@ -24,20 +33,54 @@ func TestCrud(t *testing.T) {
 	})
 
 	t.Run("CRUD for a single todo", func(t *testing.T) {
-		name := "Take out the trash"
-		id := "abc123"
-		todo, err := service.Create(ctx, id, name)
+		sampleTodo := &Todo{
+			Name:   "Take out the trash",
+			ID:     "todos:abc123",
+			Status: "todo",
+		}
+		todo, err := service.Create(ctx, sampleTodo.ID, sampleTodo.Name)
 
 		if err != nil {
 			t.Errorf("todo not created: %s", err.Error())
 			return
 		}
 
-		if todo.Name != name ||
-			todo.ID != fmt.Sprintf("todos:%s", id) ||
-			todo.Status != "todo" {
-			t.Errorf("got id:%s name:%s status:%s, want id:%s name:%s status:%s",
-				todo.ID, todo.Name, todo.Status, id, name, "todo")
+		if !todosEqual(todo, sampleTodo) {
+			t.Errorf("got %s, want %s", printTodo(todo), printTodo(sampleTodo))
+			return
 		}
+
+		readResult, err := service.One(ctx, todo.ID)
+
+		if err != nil {
+			t.Errorf("todo not read: %s", err.Error())
+			return
+		}
+
+		if !todosEqual(readResult, todo) {
+			t.Errorf("got %s, want %s", printTodo(readResult), printTodo(todo))
+			return
+		}
+
+		updateResult, err := service.Update(ctx, sampleTodo.ID, "complete")
+
+		if err != nil {
+			t.Errorf("todo not updated: %s", err.Error())
+			return
+		}
+
+		if updateResult.Status != "complete" {
+			t.Errorf("got status:%s, want status:%s", updateResult.Status, "complete")
+			return
+		}
+
+		if updateResult.CreatedDate.After(updateResult.UpdatedDate) {
+			t.Errorf("got updated_date:%s, want after:%s",
+        updateResult.UpdatedDate.Format(time.RFC3339),
+        updateResult.CreatedDate.Format(time.RFC3339))
+      return
+    }
+
+    service.Del(ctx, updateResult.ID)
 	})
 }
